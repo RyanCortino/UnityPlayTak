7567ca2979a7df21e9f3a7e5c0c4711f84e173ed
RyanCortino <RyanCortino@gmail.com>
2021-07-04 09:23:30 -0500

    Refactor Hash.cs
    I made the Hash object a static class. As the class doesn't hold data itself and simply acts upon the injected data, it made sense to have it as a public static member.

--- Tak/Assets/Scripts/Hash.cs
+++ Tak/Assets/Scripts/Hash.cs
@@ -1,28 +1,28 @@
 using System;
 
-public class Hash
+public static class Hash
 {
     public const ulong fnvBasis = 14695981039346656037;
     public const ulong fnvPrime = 1099511628211;
 
-    private ulong[] basis = new ulong[64];
+    public static ulong[] basis = new ulong[64];
     
-    private void Initialize()
+    static Hash()
     {
         UnityEngine.Random.InitState(0x7a3);
         
         for (int i = 0; i < 64; i++)
         {
-            basis[i] = Convert.ToUInt64(UnityEngine.Random.Range(0, ulong.MaxValue));      // test for negative values, panic if found
+            basis[i] = Convert.ToUInt64(UnityEngine.Random.Range(0, ulong.MaxValue));
         }
-    }    
+    }
 
-    private ulong Hash8(ulong _basis, byte _b)
+    public static ulong Hash8(ulong _basis, byte _b)
     {
         return (_basis ^ _b) * fnvPrime;
     }
 
-    private ulong Hash64(ulong _basis, ulong _w)
+    public static ulong Hash64(ulong _basis, ulong _w)
     {
         ulong h = _basis;
 
@@ -32,5 +32,16 @@ public class Hash
         h = (h ^ (_w >> 24)) * fnvPrime;
 
         return h;
+    }
+
+    public static ulong HashAt(IPosition obj, uint index)
+    {
+        if (obj.Height[(int)index] <= 1)
+            return 0;
+
+        return Hash.Hash64(
+                Hash.Hash8(basis[index], obj.Height[(int)index]),
+                obj.Stacks[(int)index]
+            );
     }
 }
